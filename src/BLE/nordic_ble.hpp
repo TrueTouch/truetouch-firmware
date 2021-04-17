8a6de17adc47a0fb48eacbfa0d819a98eb4f7e94
Cameron Kluza <cmkluza@gmail.com>
2021-04-16 23:20:50 -0400

    Removes some TODOs

--- src/BLE/nordic_ble.hpp
+++ src/BLE/nordic_ble.hpp
@@ -34,8 +34,7 @@ constexpr std::uint32_t NUS_SERVICE_UUID_TYPE { BLE_UUID_TYPE_VENDOR_BEGIN };
 constexpr std::uint32_t ADV_INTERVAL { 64 };
 
 /** The advertising duration (180 seconds) in units of 10 milliseconds. */
-// TODO CMK (1/22/21): set to 0 (no timeout)?
-constexpr std::uint32_t ADV_DURATION { 18000 };
+constexpr std::uint32_t ADV_DURATION { 0 };
 
 /** Minimum acceptable connection interval (20 ms), Connection interval uses 1.25 ms units. */
 constexpr std::uint32_t MIN_CONN_INTERVAL { MSEC_TO_UNITS(20, UNIT_1_25_MS) };
